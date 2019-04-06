d067f4243c4dc549168e75664bfe9ddec934b5eb
Michael Kennedy <mikeckennedy@gmail.com>
2019-04-05 18:04:39 -0700

    First template in place.

--- app/ch05_jinja_templates/final/pypi_org/app.py
+++ app/ch05_jinja_templates/final/pypi_org/app.py
@@ -3,9 +3,18 @@ import flask
 app = flask.Flask(__name__)
 
 
+def get_latest_packages():
+    return [
+        {'name': 'flask', 'version': '1.2.3'},
+        {'name': 'sqlalchemy', 'version': '2.2.0'},
+        {'name': 'passlib', 'version': '3.0.0'},
+    ]
+
+
 @app.route('/')
 def index():
-    return "Hello world!"
+    test_packages = get_latest_packages()
+    return flask.render_template('index.html', packages=test_packages)
 
 
 if __name__ == '__main__':

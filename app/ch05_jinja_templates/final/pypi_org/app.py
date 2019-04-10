2509d79cb7055d7b8d74793f6fd03055c7619a21
Michael Kennedy <mikeckennedy@gmail.com>
2019-04-10 14:18:02 -0700

    Add second view, first with a little nav and design.

--- app/ch05_jinja_templates/final/pypi_org/app.py
+++ app/ch05_jinja_templates/final/pypi_org/app.py
@@ -17,5 +17,10 @@ def index():
     return flask.render_template('index.html', packages=test_packages)
 
 
+@app.route('/about')
+def about():
+    return flask.render_template('about.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)

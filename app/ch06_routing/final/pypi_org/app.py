648b40982fa69f3ae6a5e1c6b308645a219795e0
Michael Kennedy <mikeckennedy@gmail.com>
2019-04-11 11:05:21 -0700

    Add popular package constrained route.

--- app/ch06_routing/final/pypi_org/app.py
+++ app/ch06_routing/final/pypi_org/app.py
@@ -12,8 +12,8 @@ def register_blueprints():
     from pypi_org.views import home_views
     from pypi_org.views import package_views
 
-    app.register_blueprint(home_views.blueprint)
     app.register_blueprint(package_views.blueprint)
+    app.register_blueprint(home_views.blueprint)
 
 
 if __name__ == '__main__':
--- app/ch06_routing/final/pypi_org/views/package_views.py
+++ app/ch06_routing/final/pypi_org/views/package_views.py
@@ -10,3 +10,9 @@ blueprint = flask.Blueprint('packages', __name__, template_folder='templates')
 # @response(template_file='packages/details.html')
 def package_details(package_name: str):
     return "Package details for {}".format(package_name)
+
+
+@blueprint.route('/<int:rank>')
+def popular(rank: int):
+    print(type(rank), rank)
+    return "The details for the {}th most popular package".format(rank)

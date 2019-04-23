b3afacfc02c0e14fbd43b6c90d3858781e238dbb
Michael Kennedy <mikeckennedy@gmail.com>
2019-04-23 14:23:41 -0700

    Now with indexes and defaults.

--- app/ch09_sqlalchmey/final/pypi_org/data/package.py
+++ app/ch09_sqlalchmey/final/pypi_org/data/package.py
@@ -1,3 +1,5 @@
+import datetime
+
 import sqlalchemy as sa
 
 from pypi_org.data.modelbase import SqlAlchemyBase
@@ -7,18 +9,18 @@ class Package(SqlAlchemyBase):
     __tablename__ = 'packages'
 
     id = sa.Column(sa.String, primary_key=True)
-    created_date = sa.Column(sa.DateTime)
-    summary = sa.Column(sa.String)
-    description = sa.Column(sa.String)
+    created_date = sa.Column(sa.DateTime, default=datetime.datetime.now, index=True)
+    summary = sa.Column(sa.String, nullable=False)
+    description = sa.Column(sa.String, nullable=True)
 
     home_page = sa.Column(sa.String)
     docs_url = sa.Column(sa.String)
     package_url = sa.Column(sa.String)
 
     author_name = sa.Column(sa.String)
-    author_email = sa.Column(sa.String)
+    author_email = sa.Column(sa.String, index=True)
 
-    license = sa.Column(sa.String)
+    license = sa.Column(sa.String, index=True)
 
     # maintainers
     # releases

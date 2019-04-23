4ad9791d3afc492f7c299931dbed2dfa08447ec5
Michael Kennedy <mikeckennedy@gmail.com>
2019-04-23 10:43:46 -0700

    Basic definition of package class.

---
+++ app/ch09_sqlalchmey/final/pypi_org/data/package.py
@@ -0,0 +1,25 @@
+import sqlalchemy as sa
+
+
+class Package:
+    __tablename__ = 'packages'
+
+    id = sa.Column(sa.String, primary_key=True)
+    created_date = sa.Column(sa.DateTime)
+    summary = sa.Column(sa.String)
+    description = sa.Column(sa.String)
+
+    home_page = sa.Column(sa.String)
+    docs_url = sa.Column(sa.String)
+    package_url = sa.Column(sa.String)
+
+    author_name = sa.Column(sa.String)
+    author_email = sa.Column(sa.String)
+
+    license = sa.Column(sa.String)
+
+    # maintainers
+    # releases
+
+    def __repr__(self):
+        return '<Package {}>'.format(self.id)

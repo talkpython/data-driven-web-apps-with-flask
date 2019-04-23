5bdeedf55bc9b6b0e9434b9f87baeea6de1547de
Michael Kennedy <mikeckennedy@gmail.com>
2019-04-23 14:00:49 -0700

    Creating tables and importing all models.

---
+++ app/ch09_sqlalchmey/final/pypi_org/data/__all_models.py
@@ -0,0 +1,4 @@
+# noinspection PyUnresolvedReferences
+from pypi_org.data.package import Package
+
+# And all the new ones...
--- app/ch09_sqlalchmey/final/pypi_org/data/db_session.py
+++ app/ch09_sqlalchmey/final/pypi_org/data/db_session.py
@@ -1,6 +1,8 @@
 import sqlalchemy as sa
 import sqlalchemy.orm as orm
 
+from pypi_org.data.modelbase import SqlAlchemyBase
+
 factory = None
 
 
@@ -14,7 +16,12 @@ def global_init(db_file: str):
         raise Exception("You must specify a db file.")
 
     conn_str = 'sqlite:///' + db_file.strip()
+    print("Connecting to DB with {}".format(conn_str))
 
     engine = sa.create_engine(conn_str, echo=False)
-
     factory = orm.sessionmaker(bind=engine)
+
+    # noinspection PyUnresolvedReferences
+    import pypi_org.data.__all_models
+
+    SqlAlchemyBase.metadata.create_all(engine)

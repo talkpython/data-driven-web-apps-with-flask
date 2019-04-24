3658851981a4f8b351c860f3e9eabc2d95a50401
Michael Kennedy <mikeckennedy@gmail.com>
2019-04-24 11:47:26 -0700

    Basic inserts ready to roll.

--- app/ch10_using_sqlachemy/final/pypi_org/data/db_session.py
+++ app/ch10_using_sqlachemy/final/pypi_org/data/db_session.py
@@ -1,15 +1,16 @@
 import sqlalchemy as sa
 import sqlalchemy.orm as orm
+from sqlalchemy.orm import Session
 
 from pypi_org.data.modelbase import SqlAlchemyBase
 
-factory = None
+__factory = None
 
 
 def global_init(db_file: str):
-    global factory
+    global __factory
 
-    if factory:
+    if __factory:
         return
 
     if not db_file or not db_file.strip():
@@ -19,9 +20,14 @@ def global_init(db_file: str):
     print("Connecting to DB with {}".format(conn_str))
 
     engine = sa.create_engine(conn_str, echo=False)
-    factory = orm.sessionmaker(bind=engine)
+    __factory = orm.sessionmaker(bind=engine)
 
     # noinspection PyUnresolvedReferences
     import pypi_org.data.__all_models
 
     SqlAlchemyBase.metadata.create_all(engine)
+
+
+def create_session() -> Session:
+    global __factory
+    return __factory()

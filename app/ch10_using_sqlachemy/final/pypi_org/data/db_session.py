62f9dfb262930de5be259e682511d3fa2334cb63
Michael Kennedy <mikeckennedy@gmail.com>
2019-05-13 15:47:52 -0700

    Add the details HTML for project details and css.

--- app/ch10_using_sqlachemy/final/pypi_org/data/db_session.py
+++ app/ch10_using_sqlachemy/final/pypi_org/data/db_session.py
@@ -19,7 +19,7 @@ def global_init(db_file: str):
     conn_str = 'sqlite:///' + db_file.strip()
     print("Connecting to DB with {}".format(conn_str))
 
-    engine = sa.create_engine(conn_str, echo=True)
+    engine = sa.create_engine(conn_str, echo=False)
     __factory = orm.sessionmaker(bind=engine)
 
     # noinspection PyUnresolvedReferences

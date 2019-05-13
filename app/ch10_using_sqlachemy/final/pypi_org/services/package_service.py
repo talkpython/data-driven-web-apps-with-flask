03065ff59d02247ce93c49143422c42466a1833a
Michael Kennedy <mikeckennedy@gmail.com>
2019-05-13 16:06:29 -0700

    Now with package details.

--- app/ch10_using_sqlachemy/final/pypi_org/services/package_service.py
+++ app/ch10_using_sqlachemy/final/pypi_org/services/package_service.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional
 import sqlalchemy.orm
 
 import pypi_org.data.db_session as db_session
@@ -10,7 +10,7 @@ def get_latest_releases(limit=10) -> List[Release]:
     session = db_session.create_session()
 
     releases = session.query(Release). \
-        options(sqlalchemy.orm.joinedload(Release.package)).\
+        options(sqlalchemy.orm.joinedload(Release.package)). \
         order_by(Release.created_date.desc()). \
         limit(limit). \
         all()
@@ -28,3 +28,21 @@ def get_package_count() -> int:
 def get_release_count() -> int:
     session = db_session.create_session()
     return session.query(Release).count()
+
+
+def get_package_by_id(package_id: str) -> Optional[Package]:
+    if not package_id:
+        return None
+
+    package_id = package_id.strip().lower()
+
+    session = db_session.create_session()
+
+    package = session.query(Package) \
+        .options(sqlalchemy.orm.joinedload(Package.releases)) \
+        .filter(Package.id == package_id) \
+        .first()
+
+    session.close()
+
+    return package
--- app/ch10_using_sqlachemy/final/pypi_org/views/package_views.py
+++ app/ch10_using_sqlachemy/final/pypi_org/views/package_views.py
@@ -7,8 +7,30 @@ blueprint = flask.Blueprint('packages', __name__, template_folder='templates')
 
 
 @blueprint.route('/project/<package_name>')
+@response(template_file='packages/details.html')
 def package_details(package_name: str):
-    return "Package details for {}".format(package_name)
+    if not package_name:
+        return flask.abort(status=404)
+
+    package = package_service.get_package_by_id(package_name.strip().lower())
+    if not package:
+        return flask.abort(status=404)
+
+    latest_version = "0.0.0"
+    latest_release = None
+    is_latest = True
+
+    if package.releases:
+        latest_release = package.releases[0]
+        latest_version = latest_release.version_text
+
+    return {
+        'package': package,
+        'latest_version': latest_version,
+        'latest_release': latest_release,
+        'release_version': latest_release,
+        'is_latest': is_latest,
+    }
 
 
 @blueprint.route('/<int:rank>')

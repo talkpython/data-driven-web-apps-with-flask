d9d723d5854b4ff6ce23c8c7b2c896c2a8209e7e
Michael Kennedy <mikeckennedy@gmail.com>
2019-05-13 14:06:01 -0700

    Now with stats slice using real data.

--- app/ch10_using_sqlachemy/final/pypi_org/services/package_service.py
+++ app/ch10_using_sqlachemy/final/pypi_org/services/package_service.py
@@ -1,6 +1,21 @@
+import pypi_org.data.db_session as db_session
+from pypi_org.data.package import Package
+from pypi_org.data.releases import Release
+
+
 def get_latest_packages():
     return [
         {'name': 'flask', 'version': '1.2.3'},
         {'name': 'sqlalchemy', 'version': '2.2.0'},
         {'name': 'passlib', 'version': '3.0.0'},
     ]
+
+
+def get_package_count() -> int:
+    session = db_session.create_session()
+    return session.query(Package).count()
+
+
+def get_release_count() -> int:
+    session = db_session.create_session()
+    return session.query(Release).count()
--- app/ch10_using_sqlachemy/final/pypi_org/views/home_views.py
+++ app/ch10_using_sqlachemy/final/pypi_org/views/home_views.py
@@ -2,6 +2,7 @@ import flask
 
 from pypi_org.infrastructure.view_modifiers import response
 import pypi_org.services.package_service as package_service
+import pypi_org.services.user_service as user_service
 
 blueprint = flask.Blueprint('home', __name__, template_folder='templates')
 
@@ -10,7 +11,12 @@ blueprint = flask.Blueprint('home', __name__, template_folder='templates')
 @response(template_file='home/index.html')
 def index():
     test_packages = package_service.get_latest_packages()
-    return {'packages': test_packages}
+    return {
+        'packages': test_packages,
+        'package_count': package_service.get_package_count(),
+        'release_count': package_service.get_release_count(),
+        'user_count': user_service.get_user_count(),
+    }
     # return flask.render_template('home/index.html', packages=test_packages)
 
 

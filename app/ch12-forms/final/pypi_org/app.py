ef2ae4ac0d5053c0781c488142190a81d5ac2f8e
Michael Kennedy <mikeckennedy@gmail.com>
2019-05-16 14:10:29 -0700

    Getting started with forms and user registration.

--- app/ch12-forms/final/pypi_org/app.py
+++ app/ch12-forms/final/pypi_org/app.py
@@ -25,9 +25,11 @@ def register_blueprints():
     from pypi_org.views import home_views
     from pypi_org.views import package_views
     from pypi_org.views import cms_views
+    from pypi_org.views import account_views
 
     app.register_blueprint(package_views.blueprint)
     app.register_blueprint(home_views.blueprint)
+    app.register_blueprint(account_views.blueprint)
     app.register_blueprint(cms_views.blueprint)
 
 

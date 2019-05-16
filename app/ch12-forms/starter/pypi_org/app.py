e4ffdd31734407e92b9522586f16e44737d16421
Michael Kennedy <mikeckennedy@gmail.com>
2019-05-16 11:24:24 -0700

    Starter code for HTML forms chapter (12).

---
+++ app/ch12-forms/starter/pypi_org/app.py
@@ -0,0 +1,35 @@
+import os
+
+import flask
+import pypi_org.data.db_session as db_session
+
+app = flask.Flask(__name__)
+
+
+def main():
+    register_blueprints()
+    setup_db()
+    app.run(debug=True)
+
+
+def setup_db():
+    db_file = os.path.join(
+        os.path.dirname(__file__),
+        'db',
+        'pypi.sqlite')
+
+    db_session.global_init(db_file)
+
+
+def register_blueprints():
+    from pypi_org.views import home_views
+    from pypi_org.views import package_views
+    from pypi_org.views import cms_views
+
+    app.register_blueprint(package_views.blueprint)
+    app.register_blueprint(home_views.blueprint)
+    app.register_blueprint(cms_views.blueprint)
+
+
+if __name__ == '__main__':
+    main()

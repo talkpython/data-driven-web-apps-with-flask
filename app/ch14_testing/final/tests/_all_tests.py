d3d806015a26e882a622366dea01b6648256ecc2
Michael Kennedy <mikeckennedy@gmail.com>
2019-06-04 14:12:07 -0700

    running outside pycharm

--- app/ch14_testing/final/tests/_all_tests.py
+++ app/ch14_testing/final/tests/_all_tests.py
@@ -1,3 +1,12 @@
+import sys
+import os
+
+container_folder = os.path.abspath(os.path.join(
+    os.path.dirname(__file__), '..'
+))
+sys.path.insert(0, container_folder)
+
+
 # noinspection PyUnresolvedReferences
 from account_tests import *
 # noinspection PyUnresolvedReferences

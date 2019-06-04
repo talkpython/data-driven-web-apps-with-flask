546c2e06af3d0c3ebb204bf34537d53985b2ab8d
Michael Kennedy <mikeckennedy@gmail.com>
2019-06-04 11:28:37 -0700

    testing registration existing user.

--- app/ch14_testing/final/tests/account_tests.py
+++ app/ch14_testing/final/tests/account_tests.py
@@ -1,3 +1,4 @@
+from pypi_org.data.users import User
 from pypi_org.viewmodels.account.register_viewmodel import RegisterViewModel
 from tests.test_client import flask_app
 import unittest.mock
@@ -27,3 +28,27 @@ def test_register_validation_when_valid():
 
     # Assert
     assert vm.error is None
+
+
+def test_register_validation_for_existing_user():
+    # 3 A's of test: Arrange, Act, then Assert
+
+    # Arrange
+    form_data = {
+        'name': 'Michael',
+        'email': 'michael@talkpython.fm',
+        'password': 'a'*6
+    }
+
+    with flask_app.test_request_context(path='/account/register', data=form_data):
+        vm = RegisterViewModel()
+
+    # Act
+    target = 'pypi_org.services.user_service.find_user_by_email'
+    test_user = User(email=form_data.get('email'))
+    with unittest.mock.patch(target, return_value=test_user):
+        vm.validate()
+
+    # Assert
+    assert vm.error is not None
+    assert 'already exists' in vm.error

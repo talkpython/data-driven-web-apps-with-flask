c728234621ad2c52535a494588dd17f7f443ced7
Michael Kennedy <mikeckennedy@gmail.com>
2019-06-10 15:53:41 -0700

    Allow configure without running for uWSGI.

--- app/ch15_deploy/final/pypi_org/app.py
+++ app/ch15_deploy/final/pypi_org/app.py
@@ -2,6 +2,7 @@ import os
 import sys
 
 import flask
+
 folder = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.insert(0, folder)
 import pypi_org.data.db_session as db_session
@@ -10,9 +11,19 @@ app = flask.Flask(__name__)
 
 
 def main():
-    register_blueprints()
-    setup_db()
+    configure()
     app.run(debug=True, port=5006)
+
+
+def configure():
+    print("Configuring Flask app:")
+
+    register_blueprints()
+    print("Registered blueprints")
+
+    setup_db()
+    print("DB setup completed.")
+    print("", flush=True)
 
 
 def setup_db():
@@ -40,3 +51,5 @@ def register_blueprints():
 
 if __name__ == '__main__':
     main()
+else:
+    configure()

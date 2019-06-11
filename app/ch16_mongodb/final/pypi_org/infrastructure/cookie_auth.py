6b7cace0752bd9f08399d281f7fa4e042973ef0e
Michael Kennedy <mikeckennedy@gmail.com>
2019-06-11 14:08:43 -0700

    Fixing login (exchange int for ObjectId).

--- app/ch16_mongodb/final/pypi_org/infrastructure/cookie_auth.py
+++ app/ch16_mongodb/final/pypi_org/infrastructure/cookie_auth.py
@@ -2,6 +2,7 @@ import hashlib
 from datetime import timedelta
 from typing import Optional
 
+import bson
 from flask import Request
 from flask import Response
 
@@ -25,7 +26,7 @@ def __add_cookie_callback(_, response: Response, name: str, value: str):
     response.set_cookie(name, value, max_age=timedelta(days=30))
 
 
-def get_user_id_via_auth_cookie(request: Request) -> Optional[int]:
+def get_user_id_via_auth_cookie(request: Request) -> Optional[bson.ObjectId]:
     if auth_cookie_name not in request.cookies:
         return None
 
@@ -41,7 +42,10 @@ def get_user_id_via_auth_cookie(request: Request) -> Optional[int]:
         print("Warning: Hash mismatch, invalid cookie value")
         return None
 
-    return try_int(user_id)
+    try:
+        return bson.ObjectId(user_id)
+    except:
+        return None
 
 
 def logout(response: Response):
--- app/ch16_mongodb/final/pypi_org/viewmodels/shared/viewmodelbase.py
+++ app/ch16_mongodb/final/pypi_org/viewmodels/shared/viewmodelbase.py
@@ -1,5 +1,6 @@
 from typing import Optional
 
+import bson
 import flask
 from flask import Request
 
@@ -12,7 +13,7 @@ class ViewModelBase:
         self.request_dict = request_dict.create('')
 
         self.error: Optional[str] = None
-        self.user_id: Optional[int] = cookie_auth.get_user_id_via_auth_cookie(self.request)
+        self.user_id: Optional[bson.ObjectId] = cookie_auth.get_user_id_via_auth_cookie(self.request)
 
     def to_dict(self):
         return self.__dict__

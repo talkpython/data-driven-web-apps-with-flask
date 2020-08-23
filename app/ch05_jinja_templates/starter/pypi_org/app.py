5e5dce66606106dd8d2ccdedb2aa012ad1ed9e55
Chris Dietrich <cdimconsulting@gmail.com>
2020-08-23 21:31:12 +0300

    vid 2 done

--- app/ch05_jinja_templates/starter/pypi_org/app.py
+++ app/ch05_jinja_templates/starter/pypi_org/app.py
@@ -2,10 +2,20 @@ import flask
 
 app = flask.Flask(__name__)
 
+def get_latest_packages():
+    return [
+        {'name': 'flask', 'version': '1.2.3'},
+        {'name': 'sqlachemy', 'version': '2.2.0'},
+        {'name': 'passlib', 'version': '3.0.0'},
+    ]
+
 @app.route("/")
 def index():
-    return "Hello World!"
+    test_packages = get_latest_packages()
+    return flask.render_template('index.html', packages=test_packages)
+
+
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)

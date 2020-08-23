d790383a7b5e24602dfea50e01d2d003877d403d
Chris Dietrich <cdimconsulting@gmail.com>
2020-08-23 22:54:40 +0300

    added stylesheets now + about template. next adding block

--- app/ch05_jinja_templates/starter/pypi_org/app.py
+++ app/ch05_jinja_templates/starter/pypi_org/app.py
@@ -1,6 +1,8 @@
 import flask
 
 app = flask.Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
 
 def get_latest_packages():
     return [
@@ -14,6 +16,9 @@ def index():
     test_packages = get_latest_packages()
     return flask.render_template('index.html', packages=test_packages)
 
+@app.route("/about")
+def about():
+    return flask.render_template('about.html')
 
 
 

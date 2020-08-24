0b42c4b38fe7c201447bdad74cebdb5b41201b04
Chris Dietrich <cdimconsulting@gmail.com>
2020-08-24 15:03:38 +0300

    chapter06: adding cms in 8 minutes: register cms_views.py blueprint, add path routing

--- app/ch06_routing/starter/pypi_org/app.py
+++ app/ch06_routing/starter/pypi_org/app.py
@@ -13,8 +13,10 @@ def main():
 def register_blueprints():
     from pypi_org.views import home_views
     from pypi_org.views import package_views
+    from pypi_org.views import cms_views
     app.register_blueprint(home_views.blueprint)
     app.register_blueprint(package_views.blueprint)
+    app.register_blueprint(cms_views.blueprint)
 
 
 if __name__ == '__main__':

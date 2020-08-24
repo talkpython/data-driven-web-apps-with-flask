44827dd06398308c99cfb475f553efaf954c63f6
Chris Dietrich <cdimconsulting@gmail.com>
2020-08-24 14:37:05 +0300

    chapter06: added new blueprint for popular route, with integer

--- app/ch06_routing/starter/pypi_org/views/package_views.py
+++ app/ch06_routing/starter/pypi_org/views/package_views.py
@@ -6,8 +6,11 @@ import pypi_org.services.package_service as package_service
 blueprint = flask.Blueprint('packages', __name__, template_folder='templates')
 
 @blueprint.route('/project/<package_name>')
-# @response(template_file='packages/details.html')
 def package_details(package_name: str):
     test_packages = package_service.get_latest_packages()
     return "Package details for {}".format(package_name)
-    # return flask.render_template('home/index.html', packages=test_packages)
+
+@blueprint.route('/<int:rank>')
+def popular(rank: int):
+    print(type(rank), rank)
+    return "The details for the {}th most popular package".format(rank)

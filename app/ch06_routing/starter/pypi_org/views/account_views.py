a850267d71ff1d728bcdb5e3000ed87b0587d1fd
Chris Dietrich <cdimconsulting@gmail.com>
2020-08-24 14:42:53 +0300

    chapter06: create new view for account_views.py for account registration and login

---
+++ app/ch06_routing/starter/pypi_org/views/account_views.py
@@ -0,0 +1,32 @@
+import flask
+
+from pypi_org.infrastructure.view_modifiers import response
+
+blueprint = flask.Blueprint('account', __name__, template_folder='templates')
+
+# ######## Index ########
+
+@blueprint.route('/account')
+@response(template_file='home/index.html')
+def index():
+    return {}
+
+
+# ######## Register ########
+
+@blueprint.route('/account/register', methods=['GET'])
+@response(template_file='account/register.html')
+def register_get():
+    return {}
+
+@blueprint.route('/account/register', methods=['POST'])
+@response(template_file='account/register.html')
+def register_get():
+    return {}
+
+# ######## Login ########
+
+@blueprint.route('/account/login', method=['GET'])
+@response(template_file='account/login.html')
+def login_get():
+    return {}

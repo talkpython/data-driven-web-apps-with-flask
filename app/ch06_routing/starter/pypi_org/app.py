36f562f17bf331b24ca39691e393373c2b7b531a
Chris Dietrich <cdimconsulting@gmail.com>
2020-08-24 14:12:04 +0300

    chapter06: using flask blueprint to register views

--- app/ch06_routing/starter/pypi_org/app.py
+++ app/ch06_routing/starter/pypi_org/app.py
@@ -4,26 +4,15 @@ import flask
 folder = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.insert(0, folder)
 
-from pypi_org.infrastructure.view_modifiers import response
-import pypi_org.services.package_service as package_service
-
 app = flask.Flask(__name__)
 
+def main():
+    register_blueprints()
+    app.run(debug=True)
 
-
-@app.route('/')
-@response(template_file='home/index.html')
-def index():
-    test_packages = package_service.get_latest_packages()
-    return {'packages': test_packages}
-    # return flask.render_template('home/index.html', packages=test_packages)
-
-
-@app.route('/about')
-@response(template_file='home/about.html')
-def about():
-    return {}
+def register_blueprints():
+    from pypi_org.views import home_views
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    main()

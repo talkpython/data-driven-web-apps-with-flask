fb2a610931bf749a664d1f51fde8fc41d4648e18
Chris Dietrich <cdimconsulting@gmail.com>
2020-08-24 14:55:28 +0300

    chapter06: adding cms in 8 minutes

---
+++ app/ch06_routing/starter/pypi_org/services/cms_service.py
@@ -0,0 +1,11 @@
+fake_db = {
+    '/company/history': {
+        'page_title': 'Company history',
+        'page_details': 'Details about company history...',
+    },
+    '/company/employees': {
+        'page_title': 'Out team',
+        'page_details': 'Details about company employees...',
+    },
+}
+

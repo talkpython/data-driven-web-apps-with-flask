064720ef4a3da70ff9be47e0bd5d22323204ae20
Chris Dietrich <cdimconsulting@gmail.com>
2020-08-27 08:22:17 +0300

    chapter09: building Package table structure

--- app/ch09_sqlalchemy/starter/data/package.py
+++ app/ch09_sqlalchemy/starter/data/package.py
@@ -1,12 +1,18 @@
 import sqlalchemy as sa
 
-class Package:
 
+class Package:
     # primary key: globally unique - we changed to string as we use the package ID as name)
     # could be an issue later to make a big migration
+    # step 1: define data types
+    # step 2: wrap them in sqlalchemy syntax
     id = sa.Column(sa.String, primary_key=True)
-    created_date =     datetime
-    desc = string
+    created_date = sa.Column(sa.DateTime)
+    summary = sa.Column(sa.String)
+    desc = sa.Column(sa.String)
 
+    home_page = sa.Column(sa.String)
+    docs_url = sa.Column(sa.String)
+    package_url = sa.Column(sa.String)
 
 
